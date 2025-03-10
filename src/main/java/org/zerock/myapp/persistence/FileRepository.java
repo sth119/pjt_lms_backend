45135401007f39d3a6a386ef32ab7e555d65659c
kongi <dbstjdal83@nate.com>
2025-03-10 16:41:54 +0900

    repository 수정

--- src/main/java/org/zerock/myapp/persistence/FileRepository.java
+++ src/main/java/org/zerock/myapp/persistence/FileRepository.java
@@ -7,16 +7,5 @@ import org.zerock.myapp.entity.File;
 @Repository
 public interface FileRepository extends JpaRepository<File, Integer> {
 
-//	//공통을 사용 가능한 LIST + CRUD 기능 수행 추상메소드 선언
-//	
-//	//검색은 어찌???/
-//	public abstract List<File> findAll();
-//	
-//	public abstract Optional<File> findById(Integer id);		//R
-//	
-//	public abstract  File save(File dto); 						//C, U
-//	
-//	public abstract void deleteById(Integer id); 				//D	
-	
 
 }//end interface
--- src/main/java/org/zerock/myapp/persistence/MemberRepository.java
+++ src/main/java/org/zerock/myapp/persistence/MemberRepository.java
@@ -2,10 +2,10 @@ package org.zerock.myapp.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.zerock.myapp.entity.Course;
+import org.zerock.myapp.entity.Member;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 	
 }//end interface
