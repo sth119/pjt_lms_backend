6137ddbdc10450228fab4ad08bbba04e39cb0d90
NogadaCoder <pardonsinau@gmail.com>
2025-03-11 17:53:00 +0900

    Update TraineeController.java

--- src/main/java/org/zerock/myapp/controller/TraineeController.java
+++ src/main/java/org/zerock/myapp/controller/TraineeController.java
@@ -1,12 +1,8 @@
 package org.zerock.myapp.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.zerock.myapp.domain.CourseDTO;
 
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
