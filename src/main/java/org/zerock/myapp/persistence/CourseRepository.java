c9baca41ec46cb0effca30301b472a5ccfde7ab5
kongi <dbstjdal83@nate.com>
2025-03-12 10:15:48 +0900

    Update CourseRepository.java

--- src/main/java/org/zerock/myapp/persistence/CourseRepository.java
+++ src/main/java/org/zerock/myapp/persistence/CourseRepository.java
@@ -3,6 +3,7 @@ package org.zerock.myapp.persistence;
 import java.awt.print.Pageable;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zerock.myapp.entity.Course;
@@ -18,37 +19,37 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 	 *  	- 강사명
 	 * */
 	//검색 리스트: 활성화상태(1) 
-	public abstract Page<Course> findByEnabled(
+	public abstract Slice<Course> findByEnabled(
 			Boolean enabled, Pageable paging
 		);
 	
 	//검색 리스트: 활성화상태(1) + 진행여부 
-	public abstract Page<Course> findByEnabledAndStatus(
+	public abstract Slice<Course> findByEnabledAndStatus(
 			Boolean enabled, Integer status, Pageable paging
 		);
 		
 	//검색 리스트: 활성화상태(1) + 진행여부 + 과정구분
-	public abstract Page<Course> findByEnabledAndStatusAndCrsType(
+	public abstract Slice<Course> findByEnabledAndStatusAndCrsType(
 			Boolean enabled, Integer status, String crsType, Pageable paging
 		);
 	
 	//검색 리스트: 활성화상태(1) + 진행여부 + 과정구분 + 과정명
-	public abstract Page<Course> findByEnabledAndStatusAndCrsTypeAndCrsNameContaining(
+	public abstract Slice<Course> findByEnabledAndStatusAndCrsTypeAndCrsNameContaining(
 			Boolean enabled, Integer status, String crsType, String crsName, Pageable paging
 		);
 	
 	//검색 리스트: 활성화상태(1) + 진행여부 + 과정구분 + 강사명  => ????
-//	public abstract Page<Course> findByEnabledAndStatusAndCrsType(
+//	public abstract Slice<Course> findByEnabledAndStatusAndCrsType(
 //			Boolean enabled, Integer state, String crsType, Pageable paging
 //		);
 	
 	//검색 리스트: 진행상태 + 과정명
-	public abstract Page<Course> findByEnabledAndStatusAndCrsNameContaining(
+	public abstract Slice<Course> findByEnabledAndStatusAndCrsNameContaining(
 			Boolean enabled, Integer status, String crsName, Pageable paging
 		);
 	
 	//검색 리스트: 진행상태 + 강사명  => ????
-//	public abstract Page<Course> findByEnabledAndStatus(
+//	public abstract Slice<Course> findByEnabledAndStatus(
 //			Boolean enabled, Integer status, Pageable paging
 //		);
 	
