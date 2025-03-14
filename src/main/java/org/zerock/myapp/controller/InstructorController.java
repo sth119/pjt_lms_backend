ff0eb1fc7ac430b96e8886df04359884c39bc148
kongi <dbstjdal83@nate.com>
2025-03-14 16:06:25 +0900

    MULTIPART

--- src/main/java/org/zerock/myapp/controller/InstructorController.java
+++ src/main/java/org/zerock/myapp/controller/InstructorController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -69,7 +70,7 @@ public class InstructorController { // 강사 관리
 	
 
 	
-	@PutMapping // 등록
+	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE) // 등록
 	Instructor register(@RequestBody InstructorDTO dto) {
 		log.info("register({}) invoked.",dto);
 		Instructor instructor = new Instructor();
