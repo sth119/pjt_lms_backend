8d59a8aa069cae77a20145d8b94f9e352b55c2ba
kongi <dbstjdal83@nate.com>
2025-03-17 01:45:48 +0900

    1

--- src/main/java/org/zerock/myapp/controller/CourseController.java
+++ src/main/java/org/zerock/myapp/controller/CourseController.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
